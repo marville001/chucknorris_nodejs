2a2ab3729edd849fd329405645861504851359ad
marville001 <mwangimartin1904@gmail.com>
2021-09-10 11:53:43 +0300

    Added error res for jokes

--- routes/jokes.js
+++ routes/jokes.js
@@ -11,7 +11,7 @@ router.get("/", async(req, res)=>{
         console.log({data});
         res.send(data)
     } catch (error) {
-        console.log(error);
+        res.status(400).send(error.message);
     }
 })
 
