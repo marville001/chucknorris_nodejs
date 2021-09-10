4f6dc4ee106bc6cca49f8ad5200c539c8b164bd9
marville001 <mwangimartin1904@gmail.com>
2021-09-10 11:33:39 +0300

    Added categories routes code

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express")
+const categoriesRoute = require("./routes/categories")
 
 const app = express();
+app.use("/categories", categoriesRoute)
 
 app.get("/", (req,res)=>{
     res.send("Weolcome to chucknoris api");
 })
+
+
 
 const PORT = process.env.port || 5001
 app.listen(PORT, ()=>console.log(`App listening on port ${PORT}`))
--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,16 @@
 const axios = require("axios")
+const express= require('express')
 
-exports.getCategories = async(req, res)=>{
+const router = express.Router();
+
+router.get("/", async(req, res)=>{
     try {
-        const {data} = axios.get('https://api.chucknorris.io/jokes/categories')
+        const {data} = await axios.get('https://api.chucknorris.io/jokes/categories')
         console.log({data});
         res.send(data)
     } catch (error) {
         console.log(error);
     }
-}
+})
+
+module.exports = router;
